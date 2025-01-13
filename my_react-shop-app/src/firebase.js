673ab9d0c1d7360593cac5c626a5874f02002f9e
jun <emfowkd@gmail.com>
2025-01-13 17:32:47 +0900

    1-9 파이어베이스 연결하기

---
+++ my_react-shop-app/src/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAYYCNAsZggj7N7WWcs22vwPoCWlD92V_A",
+  authDomain: "react-shop-app-50ab0.firebaseapp.com",
+  projectId: "react-shop-app-50ab0",
+  storageBucket: "react-shop-app-50ab0.firebasestorage.app",
+  messagingSenderId: "470935044419",
+  appId: "1:470935044419:web:9423e5e0ab3c518545aff6",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app
