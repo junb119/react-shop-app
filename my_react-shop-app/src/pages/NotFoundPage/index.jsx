4d402ce13065b7d75927b84031c7a8f722abb8a3
jun <emfowkd@gmail.com>
2025-01-15 21:59:21 +0900

    2-12 NotFoundPage 페이지 생성하기

--- my_react-shop-app/src/pages/NotFoundPage/index.jsx
+++ my_react-shop-app/src/pages/NotFoundPage/index.jsx
@@ -1,10 +1,19 @@
-
+import { Link } from "react-router-dom";
+import style from "./NotFoundPage.module.scss";
 const NotFoundPage = () => {
   return (
-    <div>
-      NotFoundPage
+    <div className="page">
+      <div className="container">
+        <div className={style.not_found}>
+          <img src="/img/not-found.png" alt="페이지를 찾을 수 없음" />
+          <p>찾고 있는 페이지를 찾을 수 없습니다.</p>
+          <p>
+            <Link to={"/"}>홈페이지로 이동</Link>
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default NotFoundPage
+export default NotFoundPage;
