35b6b8cb0a409815ecbed9d7ad617b1f0d3e8413
jun <emfowkd@gmail.com>
2025-01-15 20:17:15 +0900

    2-8 Nav 컴포넌트 기능 생성하기

--- my_react-shop-app/src/components/header/nav/Nav.jsx
+++ my_react-shop-app/src/components/header/nav/Nav.jsx
@@ -2,7 +2,27 @@ import { Link } from "react-router-dom";
 import styles from "./Nav.module.scss";
 import { FiShoppingCart, FiUser, FiLogIn } from "react-icons/fi";
 import { GoSignOut } from "react-icons/go";
+import { useAuth } from "../../../hooks/useAuth";
+import { getAuth, signOut } from "firebase/auth";
+import app from "../../../firebase";
+import { useAppdispatch, useAppSelector } from "../../../hooks/redux";
+import { removeUser } from "../../../store/user/user.slice";
+import { removeUserId } from "../../../store/cart/cart.slice";
 const Nav = () => {
+  const { isAuth } = useAuth();
+  const auth = getAuth(app);
+  const dispatch = useAppdispatch();
+  const { products } = useAppSelector((state) => state.cartSlice);
+  const handleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        dispatch(removeUser());
+        dispatch(removeUserId());
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
   return (
     <nav className={styles.nav}>
       <ul>
@@ -12,6 +32,7 @@ const Nav = () => {
               {" "}
               <FiShoppingCart />
             </Link>
+            {products.length > 0 && <b>{products.length}</b>}
           </div>
         </li>
         <li>
@@ -23,10 +44,17 @@ const Nav = () => {
           </div>
         </li>
         <li>
-          <GoSignOut className={styles.nav_sign_out} title="로그아웃" />
-          <Link to={"/login"}>
-            <FiLogIn title="로그인" />
-          </Link>
+          {isAuth ? (
+            <GoSignOut
+              className={styles.nav_sign_out}
+              title="로그아웃"
+              onClick={handleSignOut}
+            />
+          ) : (
+            <Link to={"/login"}>
+              <FiLogIn title="로그인" />
+            </Link>
+          )}
         </li>
       </ul>
     </nav>
